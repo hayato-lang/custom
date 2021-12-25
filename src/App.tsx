9de6c4844c13b636a5c25ac1c9ff4be87ce36601
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-12-26 07:55:58 +0900

    データ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { UserCard } from './components/UserCard';
+import { User } from './types/api/user';
+import { UserProfile } from './types/UserProfile';
 
 const user = {
   id: 1,
@@ -11,14 +13,26 @@ const user = {
 };
 
 function App() {
-
-  const onClickFetchUser = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users")
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => { 
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      const data = res.data.map((user) => ({
+        id: user.id,
+        name: `${user.name}(${user.username})`,
+        email: user.email,
+        address: `${user.address.city}${user.address.street}${user.address.suite}`
+      }));
+      setUserProfiles(data);
+    })
   }
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      <UserCard user={user} />
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+      
     </div>
   );
 }
